c63538dce132857faa283346ec184c39e9c175e0
SoyDiego <diegofranchina@gmail.com>
2020-04-12 05:33:02 +0200

    Added moment.js (X minutes ago...)

--- js/app.js
+++ js/app.js
@@ -10,47 +10,49 @@ fetch("https://corona.lmao.ninja/all")
 		recovered.innerHTML = data.recovered;
 		deaths.innerHTML = data.deaths;
 		activeCases.innerHTML = data.active;
-		update.innerText = `Last updated: ` + new Date(data.updated);
+		update.innerText = `Last updated: ` + moment(data.updated).fromNow();
 	});
 
 fetch("https://corona.lmao.ninja/countries/argentina")
 	.then((response) => response.json())
 	.then((data) => {
-        let flag = document.getElementById("flag");
-        let totalCases = document.getElementById("totalCases");
-        let recovered = document.getElementById("recovered");
-        let deaths = document.getElementById("deaths");
-        let todayCases = document.getElementById("todayCases");
-        let todayDeaths = document.getElementById("todayDeaths");
-        let activeCases = document.getElementById("active");
-        let update = document.getElementById("update");
-        flag.setAttribute("src", data.countryInfo.flag)
-        totalCases.innerHTML = data.cases;
-        recovered.innerHTML = data.recovered;
-        deaths.innerHTML = data.deaths;
-        todayCases.innerHTML = data.todayCases;
-        todayDeaths.innerHTML = data.todayDeaths;
-        activeCases.innerHTML = data.active;
-        update.innerText= `Last updated: ` + new Date(data.updated);
-    });
-    
+		let flag = document.getElementById("flag");
+		let totalCases = document.getElementById("totalCases");
+		let recovered = document.getElementById("recovered");
+		let deaths = document.getElementById("deaths");
+		let todayCases = document.getElementById("todayCases");
+		let todayDeaths = document.getElementById("todayDeaths");
+		let activeCases = document.getElementById("active");
+		let update = document.getElementById("update");
+		flag.setAttribute("src", data.countryInfo.flag);
+		totalCases.innerHTML = data.cases;
+		recovered.innerHTML = data.recovered;
+		deaths.innerHTML = data.deaths;
+		todayCases.innerHTML = data.todayCases;
+		todayDeaths.innerHTML = data.todayDeaths;
+		activeCases.innerHTML = data.active;
+		update.innerText =
+			`Last updated: ` + moment(data.updated).fromNow();
+	});
+
 fetch("https://corona.lmao.ninja/countries/spain")
-    .then((response) => response.json())
-    .then((data) => {
-        let flag = document.getElementById("flagES");
-        let totalCases = document.getElementById("totalCasesES");
-        let recovered = document.getElementById("recoveredES");
-        let deaths = document.getElementById("deathsES");
-        let todayCases = document.getElementById("todayCasesES");
-        let todayDeaths = document.getElementById("todayDeathsES");
-        let activeCases = document.getElementById("activeES");
-        let update = document.getElementById("updateES");
-        flag.setAttribute("src", data.countryInfo.flag);
-        totalCases.innerHTML = data.cases;
-        recovered.innerHTML = data.recovered;
-        deaths.innerHTML = data.deaths;
-        todayCases.innerHTML = data.todayCases;
-        todayDeaths.innerHTML = data.todayDeaths;
-        activeCases.innerHTML = data.active;
-        update.innerText = `Last updated: ` + new Date(data.updated);
-});
+	.then((response) => response.json())
+	.then((data) => {
+		let flag = document.getElementById("flagES");
+		let totalCases = document.getElementById("totalCasesES");
+		let recovered = document.getElementById("recoveredES");
+		let deaths = document.getElementById("deathsES");
+		let todayCases = document.getElementById("todayCasesES");
+		let todayDeaths = document.getElementById("todayDeathsES");
+		let activeCases = document.getElementById("activeES");
+		let update = document.getElementById("updateES");
+		flag.setAttribute("src", data.countryInfo.flag);
+		totalCases.innerHTML = data.cases;
+		recovered.innerHTML = data.recovered;
+		deaths.innerHTML = data.deaths;
+		todayCases.innerHTML = data.todayCases;
+		todayDeaths.innerHTML = data.todayDeaths;
+		activeCases.innerHTML = data.active;
+		update.innerText =
+			`Last updated: ` + moment(data.updated).fromNow();
+	});
